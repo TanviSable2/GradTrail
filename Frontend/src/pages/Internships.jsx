eebffb1bd1462f1c36e2ea0fb1063132ba9a3e62
tanvi <tanvisable440@gmail.com>
2026-06-28 18:16:57 +0530

    case-sensitive error fixed

--- Frontend/src/pages/Internships.jsx
+++ Frontend/src/pages/Internships.jsx
@@ -1,4 +1,6 @@
-import JobsPage from '../components/shared/JobsPage'
+import JobsPage from "../components/shared/JobsPage";
 export default function Internships() {
-  return <JobsPage jobType="internship" title="Internships" hideAiMatch={true} />
+  return (
+    <JobsPage jobType="internship" title="Internships" hideAiMatch={true} />
+  );
 }
